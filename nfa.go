660beea6a882139ab71de146d321a1c53d0905fa
Daire Ni Chathain <dairenichat@gmail.com>
2018-03-13 13:35:34 +0000

    finished thompsons

--- nfa.go
+++ nfa.go
@@ -19,33 +19,33 @@ func poregtonfa(pofix string) *nfa{
 	nfastack := []*nfa{}
 
 	//loop through exp rune at a time
-	for _, r := pxfix{
+	for _, r := range pofix{
 
 		switch r{
 		//pop 2 things off nfa stack
 		case '.':
 			//get last r off stack 
 			// LIFO order
-			frag2 := nfastack[(nfastack) -1]
+			frag2 := nfastack[len(nfastack) -1]
 			//remove popped item
-			nfastack = nfastack[:len(nfastack-1)]
-			frag1 := nfastack[(nfastack) -1]
-			nfastack = nfastack[:len(nfastack-1)]
+			nfastack = nfastack[:len(nfastack)-1]
+			frag1 :=  nfastack[len(nfastack) -1]
+			nfastack = nfastack[:len(nfastack)-1]
 
 			//concat : join poppeded items like daisy chain
-			frag1.accept.edge1 - frag2.initial
+			frag1.accept.edge1 = frag2.initial
 			//pointer
 			//push new fragment(joined frags) onto nfa stack
-			nfastack = append(nfastack, &nfa{frag1.initial, accept : frag2.accept})
+			nfastack = append(nfastack, &nfa{initial: frag1.initial, accept : frag2.accept})
 
 		case '|':
 			//get last r off stack 
 			// LIFO order
-			frag2 := nfastack[(nfastack) -1]
+			frag2 := nfastack[len(nfastack) -1]
 			//remove popped item
-			nfastack = nfastack[:len(nfastack-1)]
-			frag1 := nfastack[(nfastack) -1]
-			nfastack = nfastack[:len(nfastack-1)]
+			nfastack = nfastack[:len(nfastack)-1]
+			frag1 :=  nfastack[len(nfastack) -1]
+			nfastack = nfastack[:len(nfastack)-1]
 
 			//create two new states 
 			accept :=state{}
@@ -54,21 +54,21 @@ func poregtonfa(pofix string) *nfa{
 			frag2.accept.edge1 = &accept
 
 			//concat : join poppeded items like daisy chain
-			frag1.accept.edge1 - frag2.initial
+			frag1.accept.edge1 = frag2.initial
 			//pointer
 			//push new fragment(joined frags) onto nfa stack
 			nfastack = append(nfastack, &nfa{initial: &initial, accept :&accept})
 
 		case '*':
-			frag := nfastack[(nfastack) -1]
+			frag :=  nfastack[len(nfastack) -1]
 			//remove popped item
-			nfastack = nfastack[:len(nfastack-1)]
+			nfastack =  nfastack[:len(nfastack)-1]
 			//point at oold accpet state & new initial state
 			accept := state{}
 			//new initial state , point to old initial & new accept
 			initial := state{edge1: frag.initial, edge2: frag.accept}
-			frag2.accept.edge1 = frag.initial //old frag with 2 extra states
-			frag2.accept.edge2 = &accept
+			frag.accept.edge1 = frag.initial //old frag with 2 extra states
+			frag.accept.edge2 = &accept
 
 			nfastack = append(nfastack, &nfa{initial: &initial, accept :&accept})
 			
