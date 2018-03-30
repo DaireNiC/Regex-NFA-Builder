289f359b7f1bdc11dd83b16ad4b547a1ef0ab6ab
Daire Ni Chathain <dairenichat@gmail.com>
2018-03-30 14:22:11 +0100

    added tests for infix to postfix

--- Tests/postfixNFA_test.go
+++ Tests/postfixNFA_test.go
@@ -1,48 +1,57 @@
-package main
+/* A series of tests for the postfix notation to NFA builder
+Guidance on test structure & layout in GoLang from :
+(1) https://blog.golang.org/subtests
+(2) https://medium.com/@matryer/5-simple-tips-and-tricks-for-writing-unit-tests-in-golang-619653f90742
+*/
+package Tests
 
 import (
 	"testing"
-	"../NfaBuilder"
+	nfaBuilder "../NfaBuilder"
 )
 
-func TestPostFixToNFA(t *testing.T) {
-	//==== Test Cases ====//
-
+//Table structure for testdata
+var nfabuilderTests = []struct {
+	postfixRegex string // regex string given in post ix notation
+	n       string //input
+	expected         bool //expected result
+  }{
 	// | operator --> (OR)
-	fmt.Println("\n==== (|) operator Tests =====")
-	var bool  = NfaBuilder.PoMatch("ab|", "a")
-	fmt.Println(NfaBuilder.PoMatch("ab|", "a")) //return true
-	fmt.Println(NfaBuilder.PoMatch("ab|", "b")) //return true
-	fmt.Println(NfaBuilder.PoMatch("ab|", "ab")) //return false
-
+	{"ab|", "a", true},
+	{"ab|", "b", true},
+	{"ab|", "ab", false},
 	// . operator --> concatenation (AND)
-	fmt.Println("\n==== (.) operator Tests =====")
-	fmt.Println(NfaBuilder.PoMatch("ab.", "ab")) //return true
-	fmt.Println(NfaBuilder.PoMatch("ab.", "b")) //return false
-	fmt.Println(NfaBuilder.PoMatch("ab.", "a")) //return false
-
+	{"ab.", "ab", true},
+	{"ab.", "b", false},
+	{"ab.", "a", false},
 	// + operator --> one or more
-	fmt.Println("\n==== (+) operator Tests =====")
-	fmt.Println(NfaBuilder.PoMatch("10.1+|", "1")) //return true
-	fmt.Println(NfaBuilder.PoMatch("10.1+|", "111111")) //return true
-	fmt.Println(NfaBuilder.PoMatch("10.1+|", "")) //return false
-
+	{"10.1+|", "", false},
+	{"10.1+|", "1", true},
+	{"10.1+|", "1111", true},
 	// ? operator --> zero or one
-	fmt.Println("\n==== (?) operator Tests =====")
-	fmt.Println(NfaBuilder.PoMatch("10.1?|", "")) //return true
-	fmt.Println(NfaBuilder.PoMatch("10.1?|", "1")) //return true
-	fmt.Println(NfaBuilder.PoMatch("10.1?|", "111")) //return 
-	
-
+	{"10.1?|", "", true},
+	{"10.1?|", "1", true},
+	{"10.1?|", "1111", false},
 	// * Kleene star --> zero or more	
-	fmt.Println("\n==== Kleene Star Tests =====")
-	fmt.Println(NfaBuilder.PoMatch("10.1*|", "")) //return true
-	fmt.Println(NfaBuilder.PoMatch("10.1+|", "1")) //return true
-	fmt.Println(NfaBuilder.PoMatch("10.1+|", "111")) //return true
-	fmt.Println(NfaBuilder.PoMatch("10.1+|", "0")) //return false
-
+	{"10.1*|", "", true},
+	{"10.1*|", "1", true},
+	{"10.1*|", "1111", true},
+	{"10.1*|", "0", false},
 	// Combined Expressions Tests
-	fmt.Println("\n==== Combined Expressions Tests =====")
-	fmt.Println(NfaBuilder.PoMatch("ab.f*|", "ffffff")) //return true
-	fmt.Println(NfaBuilder.PoMatch("eP.10.Q.q.*|", "")) //return true
-}
+	{"ab.f*|", "ffffff", true},
+	{"eP.9*|", "999", true},
+	{"eP.9*|", "EPPPPPPPPPPPPPPpp", false},
+
+  }
+
+/*	Tests the NFA's built from the given postFix regex
+ 	with input string (n) & asserts true/false
+ */
+ func TestNFABuilder(t *testing.T) {
+	for _, tt := range nfabuilderTests {
+		actual := nfaBuilder.PoMatch(tt.postfixRegex, tt.n)
+		if actual != tt.expected {
+		  t.Errorf("\nPostfix Regex: %q\nInput String: %q\nExpected: %b, \nActual %b", tt.postfixRegex, tt.n, tt.expected, actual)
+		}
+	  }
+	}
