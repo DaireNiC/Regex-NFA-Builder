996b9009819aca350280edbe5fa90433e0e0f14e
Daire Ni Chathain <dairenichat@gmail.com>
2018-03-30 14:59:10 +0100

    completed main.go

--- regToNFA.go
+++ regToNFA.go
@@ -2,19 +2,16 @@ package main
 
 import (
 	"fmt"
-	//postfixMaker "./utils"
-	NfaBuilder "./NfaBuilder"
+	infixToPostfix "./InfixToPostfix"
+	nfaBuilder "./NfaBuilder"
 	
 
 )
 func main(){
-	//test1
-//	fmt.Println(NfaBuilder.PoMatch("a|c*b", "ab"))
-	fmt.Println(NfaBuilder.PoMatch("ab.c?|", "c")) //return true
-//	fmt.Println(NfaBuilder.PoMatch("a+c*|", "ac"))
-//	fmt.Println(NfaBuilder.PoMatch(postfixMaker.IntoPost("a?b?c?"), "abc"))
-//	fmt.Println(NfaBuilder.PoMatch("ab.c+", "ac"))
-//	fmt.Println(NfaBuilder.PoMatch("ab?c*", "abcc"))
-//	fmt.Println(NfaBuilder.PoMatch("ab?c*", "abbcc"))
-	
+	//==== Test Cases ====//
+	postfix := infixToPostfix.IntoPost("(a.b)|c")
+	//output should be : "ab.c*"
+	fmt.Println(postfix)
+	// Evalutation is true given input
+	fmt.Println(nfaBuilder.PoMatch(postfix, "ab")) //return true
 }
