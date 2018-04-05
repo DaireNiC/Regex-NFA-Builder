e314290cb6d0ce47f9ba0bf85fd8ff2461be50ce
Daire Ni Chathain <dairenichat@gmail.com>
2018-04-05 07:01:09 +0100

    added more comments for clarity

--- infixToPostfix/infixToPost_test.go
+++ infixToPostfix/infixToPost_test.go
@@ -1,4 +1,7 @@
-/* A series of tests for the infix to postfix notation converter
+/* 
+---------------------------------------------------------------
+A series of tests for the infix to postfix notation converter
+---------------------------------------------------------------
 Guidance on test structure & layout in GoLang from :
 (1) https://blog.golang.org/subtests
 (2) https://medium.com/@matryer/5-simple-tips-and-tricks-for-writing-unit-tests-in-golang-619653f90742
